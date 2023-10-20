5485dbe4a9d2305b9c7b2d45401fbc27ee3f515c
teflon40 <frimpong.bernard3220@gmail.com>
2023-10-20 00:49:06 +0000

    Sub opcode

--- math_ops.c
+++ math_ops.c
@@ -16,3 +16,21 @@ void Add(stack_t **stack, unsigned int number)
 	sum = fpop(stack) + fpop(stack);
 	fpush(stack, sum);
 }
+
+/**
+ * Sub - performs subtraction operations
+ *
+ * @stack: a stack.
+ * @number: line number.
+ */
+void Sub(stack_t **stack, unsigned int number)
+{
+	int difference, tmp;
+
+	if (stackLen(*stack) < 2)
+		ErrExit(*stack, "L%d: can't sub, stack too short\n", number);
+
+	tmp = fpop(stack);
+	difference = fpop(stack) - tmp;
+	fpush(stack, difference);
+}
